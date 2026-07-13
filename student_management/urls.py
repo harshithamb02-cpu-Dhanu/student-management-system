ab15cf741bc67fa808f86f02407eebefb5da8f72
Harshitha MB <harshithamb02@gmail.com>
2026-07-13 12:25:41 +0530

    Fixed login API and updated project

--- student_management/urls.py
+++ student_management/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.contrib import admin
-from django.urls import path, include
-from .views import home
 urlpatterns = [
-    path("",home),
-    path("admin/", admin.site.urls),
-    path("api/students/", include("students.urls")),
+    path('admin/', admin.site.urls),
+
+    path('api/', include('accounts.urls')),
+    path('api/students/', include('students.urls')),
+    path('api/faculty/', include('faculty.urls')),
+    path('api/courses/', include('courses.urls')),
 ]
